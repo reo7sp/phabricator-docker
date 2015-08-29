dd57c039a3efc8c9f4ad0ad2453d8de922444eb0
Oleg Morozenkov <omorozenkov@gmail.com>
2015-08-29 23:38:16 +0300

    Set mysql host at start

--- phabricator-run.sh
+++ phabricator-run.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-/usr/src/phabricator/bin/storage upgrade --force
+cd /usr/src/phabricator
+bin/config set mysql.host "$MYSQL_PORT_3306_TCP_ADDR:$MYSQL_PORT_3306_TCP_PORT"
+bin/storage upgrade --force
+
 /usr/bin/supervisord -c /etc/supervisor.conf
