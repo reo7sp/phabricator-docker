4da0bc35ef0b64555bee1235fa21fe93e700ddfc
Oleg Morozenkov <omorozenkov@gmail.com>
2015-08-30 20:38:19 +0300

    Fix start

--- mysql-wait.sh
+++ mysql-wait.sh
@@ -2,16 +2,15 @@
 
 if [ -z "$1" -a -z "$2" -a -z "$3" ]
 then
-	echo >&2 "Usage: /mysql-wait.sh USER PASS HOST"
+	echo >&2 "Usage: /mysql-wait.sh USER PASS HOST PORT"
 	exit 1
 fi
 
-echo -n "mysql is starting"
+echo "mysql is starting..."
 i=60
 while [ $i -ne 0 ]
 do
-	echo -n "."
-	echo "SELECT 1" | mysql -u $1 -p $2 -h $3 > /dev/null 2> /dev/null
+	echo "SELECT 1" | mysql -u $1 -p $2 -h $3 --port $4
 	if [ $? -eq 0 ]
 	then
 		exit 0
@@ -19,9 +18,10 @@ do
 	sleep 1
 	i=$((i-1))
 done
-echo
 if [ "$i" -eq 0 ]
 then
 	echo >&2 "mysql failed to start."
 	exit 1
+else
+	echo "Done"
 fi
--- phabricator-run.sh
+++ phabricator-run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-set -e
 cd /usr/src/phabricator
 
 bin/config set mysql.host $MYSQL_PORT_3306_TCP_ADDR:$MYSQL_PORT_3306_TCP_PORT
-
-sleep 15
-sh /mysql-wait.sh $(bin/config get mysql.user) $(bin/config get mysql.pass) $(bin/config get mysql.host)
+sleep 5
+mysqluser=$(bin/config get mysql.user | jq ".config[0].value" | sed 's/^"//g; s/"$//g')
+mysqlpass=$(bin/config get mysql.pass | jq ".config[0].value" | sed 's/^"//g; s/"$//g')
+sh /mysql-wait.sh $mysqluser $mysqlpass $MYSQL_PORT_3306_TCP_ADDR $MYSQL_PORT_3306_TCP_PORT
 bin/storage upgrade --force
 
 /usr/bin/supervisord -c /etc/supervisor.conf
