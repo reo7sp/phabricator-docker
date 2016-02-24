2bbd1629b5b4cc64b4a2c57e7e33330e218957fd
Oleg Morozenkov <omorozenkov@gmail.com>
2016-02-24 22:42:21 +0300

    Allow custom mysql setup

--- main.sh
+++ main.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+set -e
+
 cd /usr/src/phabricator
 
-bin/config set mysql.host $MYSQL_PORT_3306_TCP_ADDR
-bin/config set mysql.port $MYSQL_PORT_3306_TCP_PORT
+if [[ "$PHABRICATOR_DOCKER_DONT_SET_MYSQL" == 1 ]]; then
+	bin/config set mysql.host $MYSQL_PORT_3306_TCP_ADDR
+	bin/config set mysql.port $MYSQL_PORT_3306_TCP_PORT
+fi
 
 mkdir -p /var/lib/phabricator/repo
 chown -R phabricator-daemon /var/lib/phabricator/repo
@@ -19,7 +23,9 @@ sleep 5
 
 mysqluser=$(bin/config get mysql.user | jq ".config[0].value" | sed 's/^"//g; s/"$//g')
 mysqlpass=$(bin/config get mysql.pass | jq ".config[0].value" | sed 's/^"//g; s/"$//g')
-sh /mysql-wait.sh $mysqluser $mysqlpass $MYSQL_PORT_3306_TCP_ADDR $MYSQL_PORT_3306_TCP_PORT
+mysqlhost=$(bin/config get mysql.host | jq ".config[0].value" | sed 's/^"//g; s/"$//g')
+mysqlport=$(bin/config get mysql.port | jq ".config[0].value" | sed 's/^"//g; s/"$//g')
+sh /mysql-wait.sh $mysqluser $mysqlpass $mysqlhost $mysqlport
 echo y | bin/storage upgrade --force
 
 /usr/bin/supervisord -c /etc/supervisor.conf
